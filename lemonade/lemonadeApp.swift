99209cd73c2bdf949946e1af819fb3bb3165a920
danaschanker <126301091+danaschanker@users.noreply.github.com>
2023-02-27 16:07:52 -0800

    Initial Commit

---
+++ lemonade/lemonadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  lemonadeApp.swift
+//  lemonade
+//
+//  Created by Dana Schanker on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct lemonadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
